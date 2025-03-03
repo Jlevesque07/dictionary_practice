ba48e01cd2a8b93fdbb82972b1dbf28b996adce4
Jlevesque07 <joshual251@deltalearns.ca>
2025-03-03 08:49:25 -0800

    Added Files

---
+++ People.py
@@ -0,0 +1,3 @@
+# 2. PEOPLE: Make 3 dictionaries representing different people (their firstname, lastname, and phone number) and store all three dictionaries in a list called "people". 
+# Loop through your list of people, printing everything you know about each person. 
+# Make sure the final output is nicely formatted and neat.
---
+++ Pets.py
@@ -0,0 +1,5 @@
+# 1. PETS: Make 3 dictionaries where each dictionary represents a different pet. 
+# In each dictionary, include the kind of animal and the owner's name. 
+# Store these dictionaries in a list called "pets". 
+# Next, loop through your list and as you do, print everything you know about each pet. 
+# Make sure the final output is nicely formatted and neat.
