09b688095e53340cf5278b6aa8e4572855cd62a6
Jlevesque07 <joshual251@deltalearns.ca>
2025-03-03 09:41:43 -0800

    Working on Cities

--- Citites.py
+++ Citites.py
@@ -1,5 +1,17 @@
 # 3. CITIES: Make a dictionary called "cities". 
-# Use the names of three cities as keys in your dictionary. 
+# Use the names of three cities as keys in your dictionary.
+cities = ['Alhaio', 'Applecity', 'Digwoid']
+weathers = ["sunny", "rainy", "cloudy"]
+stuff = []
 # Create a dictionary of information about each city and include the country that the city is in, its approximate population, and one fact about that city. 
+for key in cities:
+    key = {}
+    print()
+    key['country'] = input("What country is this city in? ").strip().title
+    key['population'] = int(input("What is the population of this city? "))
+    key['funfact'] = input("What is a fun fact about the city?" ).strip().title()
+    stuff.append(key)
 # The keys for each city's dictionary should be something like "country, population, and fact". Print the name of each city and all of the information you have stored about it. 
 # Make sure the final output is nicely formatted and neat
+
+print(stuff)
